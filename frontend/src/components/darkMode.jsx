49d90badaff427d59fb556fd4090e1376d1a333b
JuanDa2006 <86136478+JuanDa2006@users.noreply.github.com>
2024-11-01 21:35:56 +0100

    Up

---
+++ frontend/src/components/darkMode.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
+
+
+const ToggleDarkMode = () => {
+    const [darkMode, setDarkMode] = useState(() => {
+        const savedTheme = localStorage.getItem('theme');
+        if (savedTheme) {
+            return savedTheme === 'dark';
+        }
+
+        return (
+            window.matchMedia &&
+            window.matchMedia('(prefers-color-scheme: dark)').matches
+        );
+    });
+
+    const toggleTheme = () => {
+        const newTheme = !darkMode;
+        setDarkMode(newTheme);
+        localStorage.setItem('theme', newTheme ? 'dark' : 'light');
+        document.body.classList.toggle('dark', newTheme);
+    };
+
+    useEffect(() => {
+        document.body.classList.toggle('dark', darkMode);
+    }, [darkMode]);
+
+    return (
+        <>
+            <button>
+                
+            </button>
+        </>
+    )
+};
+
+export default ToggleDarkMode;
--- frontend/src/pages/test.jsx
+++ frontend/src/pages/test.jsx
@@ -1,7 +1,11 @@
+import { Flowbite } from "flowbite-react";
+import ToggleDarkMode from "../components/darkMode";
+
 export default function Test() {
     return (
-        <>
+        <Flowbite>
             <h1>Test</h1>
-        </>
+            <ToggleDarkMode />
+        </Flowbite>
     )
 }
