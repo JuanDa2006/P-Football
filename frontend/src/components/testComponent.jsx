dd8ae202040daf583869d997fb40b1ce9ffb58b4
JuanDa2006 <86136478+JuanDa2006@users.noreply.github.com>
2024-11-06 13:06:14 +0100

    Update testComponent.jsx

--- frontend/src/components/testComponent.jsx
+++ frontend/src/components/testComponent.jsx
@@ -1,17 +1,7 @@
-import { Card } from 'flowbite-react';
-import LoremSM from '../utils/loremSm';
-import CountryFlag from './countryFlag';
-
 export default function TestComponent() {
     return (
         <>
-            <Card className='max-w-sm'>
-                <div className='flex justify-between content-center w-full h-auto'>
-                    <h5 className='text-2xl font-bold text-black dark:text-white'>Card</h5>
-                    <CountryFlag countryId={'COL'} />
-                </div>
-                <LoremSM />
-            </Card>
+            
         </>
     );
 }
