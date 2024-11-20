2af5f0db188ba5b5cc120bf4a415030bd6ae3805
JuanDa2006 <86136478+JuanDa2006@users.noreply.github.com>
2024-11-20 10:05:50 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './input.css';
 import RoutesConfig from './routes/routes';
-import BaseLayout from './layouts/baseLayout';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
