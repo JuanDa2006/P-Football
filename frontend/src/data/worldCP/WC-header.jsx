4ce5bcfc8aabf6bf76bc1825bc4c727b8e8358c3
JuanDa2006 <86136478+JuanDa2006@users.noreply.github.com>
2024-11-20 12:49:23 +0100

    Update WC-header.jsx

--- frontend/src/data/worldCP/WC-header.jsx
+++ frontend/src/data/worldCP/WC-header.jsx
@@ -2,112 +2,90 @@ const header_wc = [
     {
         id: '1930',
         ganador: 'Uruguay',
-        header: 'El primer Mundial de la FIFA se jugó en 1930 en Uruguay, la cual fue la campeona del sertamen.',
     },
     {
         id: '1934',
         ganador: 'Italia',
-        header: 'El Mundial de 1934 se jugó en Italia y marcó la primera edición con un torneo de eliminación directa desde la primera ronda, sin fase de grupos.',
     },
     {
         id: '1938',
         ganador: 'Italia',
-        header: 'El Mundial de 1938 se celebró en Francia y fue el último antes de la pausa por la Segunda Guerra Mundial. Aquí tienes el resumen y análisis de Italia, que repitió como campeona.',
     },
     {
         id: '1950',
         ganador: 'Uruguay',
-        header: '',
     },
     {
         id: '1954',
         ganador: 'Alemania',
-        header: '',
     },
     {
         id: '1958',
         ganador: 'Brasil',
-        header: '',
     },
     {
         id: '1962',
         ganador: 'Brail',
-        header: '',
     },
     {
         id: '1966',
         ganador: 'Inglaterra',
-        header: '',
     },
     {
         id: '1970',
         ganador: 'Brail',
-        header: '',
     },
     {
         id: '1974',
         ganador: 'Alemania',
-        header: '',
     },
     {
         id: '1978',
         ganador: 'Argentina',
-        header: '',
     },
     {
         id: '1982',
         ganador: 'Italia',
-        header: '',
     },
     {
         id: '1986',
         ganador: 'Argentina',
-        header: '',
     },
     {
         id: '1990',
         ganador: 'Alemania',
-        header: '',
     },
     {
         id: '1994',
         ganador: 'Brasil',
-        header: '',
     },
     {
         id: '1998',
         ganador: 'Francia',
-        header: '',
     },
     {
         id: '2002',
         ganador: 'Brasil',
-        header: '',
     },
     {
         id: '2006',
         ganador: 'Italia',
-        header: '',
     },
     {
         id: '2010',
         ganador: 'España',
-        header: 'El Mundial de 2010 se celebró en Sudáfrica, siendo la primera vez que el torneo se llevó a cabo en el continente africano. España se coronó campeona, logrando su primer título mundial al vencer a los Países Bajos en una final emocionante y disputada.',
     },
     {
         id: '2014',
         ganador: 'Alemania',
-        header: '',
     },
     {
         id: '2018',
         ganador: 'Francia',
-        header: '',
     },
     {
         id: '2022',
         ganador: 'Argentina',
-        header: '',
     },
 ];
 
