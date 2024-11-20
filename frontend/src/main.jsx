6f79cf1b8b6853ad185262e8545d3c41e717447d
JuanDa2006 <86136478+JuanDa2006@users.noreply.github.com>
2024-11-20 09:49:00 +0100

    Up

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-    createBrowserRouter,
-    RouterProvider,
     BrowserRouter,
-    Router,
-    Routes,
 } from 'react-router-dom';
 import './input.css';
+import RoutesConfig from './routes/routes';
+import BaseLayout from './layouts/baseLayout';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-    
-    </React.StrictMode>
+    <BrowserRouter>
+            <RoutesConfig />
+    </BrowserRouter>
 );
--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -3,36 +3,34 @@ import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 // Routes
-const Home = lazy(() => import('../pages/mundiales/1930-Uruguay'));
-
-import Home from '../pages/home';
-import Mundiales from '../pages/mundiales';
-import Test from '../pages/test';
+const Home = lazy(() => import('../pages/home'));
+const ErrorPage = lazy(() => import('../error/errorPage'));
+const Test = lazy(() => import('../pages/test'));
 
 // Mundiales
-import URU_1930 from '../pages/mundiales/1930-Uruguay';
-import ITA_1934 from '../pages/mundiales/1934-Italia';
-import FRA_1938 from '../pages/mundiales/1938-Francia';
-import BRA_1950 from '../pages/mundiales/1950-Brasil';
-import SUI_1954 from '../pages/mundiales/1954-Suiza';
-import SWE_1958 from '../pages/mundiales/1958-Suecia';
-import CHI_1962 from '../pages/mundiales/1962-Chile';
-import ENG_1966 from '../pages/mundiales/1966-Inglaterra';
-import MEX_1970 from '../pages/mundiales/1970-México';
-import GER_1974 from '../pages/mundiales/1974-Alemania';
-import ARG_1978 from '../pages/mundiales/1978-Argentina';
-import ESP_1982 from '../pages/mundiales/1982-España';
-import MEX_1986 from '../pages/mundiales/1986-México';
-import ITA_1990 from '../pages/mundiales/1990-Italia';
-import USA_1994 from '../pages/mundiales/1994-Estados-Unidos';
-import FRA_1998 from '../pages/mundiales/1998-Francia';
-import KOR_JAP_2002 from '../pages/mundiales/2002-Corea-Japon';
-import GER_2006 from '../pages/mundiales/2006-Alemania';
-import SUD_2010 from '../pages/mundiales/2010-Sudafrica';
-import BRA_2014 from '../pages/mundiales/2014-Brasil';
-import RUS_2018 from '../pages/mundiales/2018-Rusia';
-import QAT_2022 from '../pages/mundiales/2022-Catar';
-
+const Mundiales = lazy(() => import('../pages/mundiales'));
+const URU_1930 = lazy(() => import('../pages/mundiales/1930-Uruguay'));
+const ITA_1934 = lazy(() => import('../pages/mundiales/1934-Italia'));
+const FRA_1938 = lazy(() => import('../pages/mundiales/1938-Francia'));
+const BRA_1950 = lazy(() => import('../pages/mundiales/1950-Brasil'));
+const SUI_1954 = lazy(() => import('../pages/mundiales/1954-Suiza'));
+const SWE_1958 = lazy(() => import('../pages/mundiales/1958-Suecia'));
+const CHI_1962 = lazy(() => import('../pages/mundiales/1962-Chile'));
+const ENG_1966 = lazy(() => import('../pages/mundiales/1966-Inglaterra'));
+const MEX_1970 = lazy(() => import('../pages/mundiales/1970-México'));
+const GER_1974 = lazy(() => import('../pages/mundiales/1974-Alemania'));
+const ARG_1978 = lazy(() => import('../pages/mundiales/1978-Argentina'));
+const ESP_1982 = lazy(() => import('../pages/mundiales/1982-España'));
+const MEX_1986 = lazy(() => import('../pages/mundiales/1986-México'));
+const ITA_1990 = lazy(() => import('../pages/mundiales/1990-Italia'));
+const USA_1994 = lazy(() => import('../pages/mundiales/1994-Estados-Unidos'));
+const FRA_1998 = lazy(() => import('../pages/mundiales/1998-Francia'));
+const KOR_JAP_2002 = lazy(() => import('../pages/mundiales/2002-Corea-Japon'));
+const GER_2006 = lazy(() => import('../pages/mundiales/2006-Alemania'));
+const SUD_2010 = lazy(() => import('../pages/mundiales/2010-Sudafrica'));
+const BRA_2014 = lazy(() => import('../pages/mundiales/2014-Brasil'));
+const RUS_2018 = lazy(() => import('../pages/mundiales/2018-Rusia'));
+const QAT_2022 = lazy(() => import('../pages/mundiales/2022-Catar'));
 // Const
 function Loading() {
     return <div>Loadding...</div>;
@@ -42,8 +40,35 @@ function RoutesConfig() {
     return (
         <Suspense fellback={<Loading />}>
             <Routes>
-                <Route path='Home' element={<Home />} />
+                <Route path='/' element={<Home />} />
+                <Route path='/test' element={<Test />} />
+                <Route path='/mundiales' element={<Mundiales />} />
+                <Route path='/mundiales/1930' element={<URU_1930 />} />
+                <Route path='' element={<ITA_1934 />} />
+                <Route path='' element={<FRA_1938 />} />
+                <Route path='' element={<BRA_1950 />} />
+                <Route path='' element={<SUI_1954 />} />
+                <Route path='' element={<SWE_1958 />} />
+                <Route path='' element={<CHI_1962 />} />
+                <Route path='' element={<ENG_1966 />} />
+                <Route path='' element={<MEX_1970 />} />
+                <Route path='' element={<GER_1974 />} />
+                <Route path='' element={<ARG_1978 />} />
+                <Route path='' element={<ESP_1982 />} />
+                <Route path='' element={<MEX_1986 />} />
+                <Route path='' element={<ITA_1990 />} />
+                <Route path='' element={<USA_1994 />} />
+                <Route path='' element={<FRA_1998 />} />
+                <Route path='' element={<KOR_JAP_2002 />} />
+                <Route path='' element={<GER_2006 />} />
+                <Route path='/mundiales/2010' element={<SUD_2010 />} />
+                <Route path='' element={<BRA_2014 />} />
+                <Route path='' element={<RUS_2018 />} />
+                <Route path='' element={<QAT_2022 />} />
+                <Route path='*' element={<ErrorPage />} />
             </Routes>
         </Suspense>
-    )
+    );
 }
+
+export default RoutesConfig;
